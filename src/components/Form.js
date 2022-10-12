a2e5e3b6d0ca3df6dac8e161d63cc57a967acb1b
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-10-12 21:18:37 +0100

    logistics-website

---
+++ src/components/Form.js
@@ -0,0 +1,111 @@
+
+import React from "react";
+
+const Form = () => {
+  return (
+    // <section className='section'>
+    // <div className='container mx-auto'>
+    //         <form action="https://getform.io/f/{your-form-endpoint}" method="POST" enctype="multipart/form-data">
+    //             <input type='text' name='name' placeholder='Name'/>
+    //             <input type='number' name='message' placeholder='Number'/>
+    //             <input type='email' name='email' placeholder='Email'/>
+    //             <input type='text' name='message' placeholder='Address'/>
+    //             <textarea name='message' type='text' placeholder='Message' cols='30' row='10'/>
+    //             <button type='submit'>Submit</button>
+
+    //         </form>
+    // </div>
+
+    // </section>
+    <section class="text-gray-600 body-font section">
+      <div class="container px-5 pt-24  mx-auto flex flex-wrap justify-start">
+        <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 items-center">
+        <h2
+          className='title mb-5 lg:mb-10 text-center max-w-[920px] mx-auto'
+          data-aos='fade-up'
+          data-aos-delay='200'
+        >
+           Why don't you give us a try
+          </h2>
+          <p class="leading-relaxed text-center mb-3 text-[30px] font-medium"
+            data-aos='fade-up'
+            data-aos-delay='200'>
+            We're <em>READY</em> to <em>RUN</em> for <em>YOU!</em>
+          </p>
+        </div>
+        <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"         data-aos='fade-up'
+        data-aos-delay='200'>
+          <h2 class="text-gray-900 text-lg text-[30px] font-medium title-font mb-5">
+            Sign Up
+          </h2>
+          <form action="https://getform.io/f/7602fbcb-ef2f-46c9-9486-08de1e68b08d" method="POST" id='formlink'>
+          <div class="relative mb-4">
+            <label for="full-name" class="leading-7 text-sm text-gray-600">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="full-name"
+              name="full-name"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="number" class="leading-7 text-sm text-gray-600">
+              Phone Number
+            </label>
+            <input
+              type="number"
+              id="number"
+              name="number"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="address" class="leading-7 text-sm text-gray-600">
+              Address
+            </label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="message" class="leading-7 text-sm text-gray-600">
+              Message
+            </label>
+            <textarea cols='30' row='20' name="message" type="text" class="w-full h-[128px] bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            {/* <input
+              type="text"
+              id="message"
+              name="message"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            /> */}
+          </div>
+          <button className='btn btn-sm bg-accent hover:bg-accentHover w-[102px] text-white'>
+            Submit
+          </button>
+          </form>
+          <p class="text-xs text-gray-500 mt-3">
+            Literally Always Ready.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
---
+++ src/data.js
@@ -0,0 +1,276 @@
+// import images
+
+import HeroImg from '../src/assets/img/hero/image.svg';
+import OverviewProductImg from '../src/assets/img/overview/product.svg';
+import FacebookImg from '../src/assets/img/overview/brands/facebook.svg';
+import GoogleImg from '../src/assets/img/overview/brands/google.svg';
+import CocaColaImg from '../src/assets/img/overview/brands/coca-cola.svg';
+import LinkedInImg from '../src/assets/img/overview/brands/linkedin.svg';
+import SamsungImg from '../src/assets/img/overview/brands/samsung.svg';
+import Feature1Img from '../src/assets/img/features/feature1-img.svg';
+import Feature2Img from '../src/assets/img/features/feature2-img.svg';
+import Feature3Img from '../src/assets/img/features/feature3-img.svg';
+import ArrowRightImg from '../src/assets/img/features/arrow-right.svg';
+import CardIconImg1 from '../src/assets/img/product/cards/icon1.svg';
+import CardIconImg2 from '../src/assets/img/product/cards/icon2.svg';
+import CardIconImg3 from '../src/assets/img/product/cards/icon3.svg';
+import PricingIcon1 from '../src/assets/img/pricing/icon1.svg';
+import PricingIcon2 from '../src/assets/img/pricing/icon2.svg';
+import PricingIcon3 from '../src/assets/img/pricing/icon3.svg';
+import AvatarImg1 from '../src/assets/img/testimonial/avatar1.png';
+import AvatarImg2 from '../src/assets/img/testimonial/avatar2.png';
+import AvatarImg3 from '../src/assets/img/testimonial/avatar3.png';
+import AvatarImg4 from '../src/assets/img/testimonial/avatar4.png';
+import AvatarImg5 from '../src/assets/img/testimonial/avatar5.png';
+import CtaImg1 from '../src/assets/img/cta/image1.svg';
+import CtaImg2 from '../src/assets/img/cta/image2.svg';
+import WhatsappIcon from '../src/assets/img/copyright/icons8-whatsapp.svg';
+import GmailIcon from '../src/assets/img/copyright/icons8-gmail-logo.svg';
+import PhoneIcon from '../src/assets/img/copyright/icons8-phone.svg';
+
+export const header = {
+  logo: 'Treasure Logistics',
+  btnText: 'Request a Delivery',
+};
+
+export const nav = [
+  { name: 'About us', href: '#feature' },
+  { name: 'Features', href: '#cta' },
+  { name: 'Location', href: '#product'},
+  { name: 'Feedback', href: '#testimonial' },
+];
+
+export const hero = {
+  title: 'Lets SUPPORT your BUSINESS',
+  subtitle: 'Helps you to organize and deliver',
+  btnText: 'Place Order',
+  compText: '— Web, iOS and Android',
+  image: HeroImg,
+};
+
+export const overview = {
+  productImg: OverviewProductImg,
+  brands: [
+    {
+      image: FacebookImg,
+      delay: 300,
+    },
+    {
+      image: GoogleImg,
+      delay: 400,
+    },
+    {
+      image: CocaColaImg,
+      delay: 500,
+    },
+    {
+      image: LinkedInImg,
+      delay: 600,
+    },
+    {
+      image: SamsungImg,
+      delay: 700,
+    },
+  ],
+};
+
+export const features = {
+  feature1: {
+    pretitle: 'Alwalys ontime',
+    title: 'Real-time Delivery',
+    subtitle:
+      'Send your Documents, Merchandise, Textile Material (AsoEbi), Birthday Cards/Presents, Christmas Cards/Presents, Party Invitations, Hampers, Gift Baskets, Wedding Invitations, Diaries, Calendars and Anniversary Presents etc. to any location of your choice',
+    btnLink: 'Learn more',
+    btnIcon: ArrowRightImg,
+    image: Feature1Img,
+  },
+  feature2: {
+    pretitle: 'Free some cost',
+    title: 'Save cost for you',
+    subtitle:
+      'You spend less when you deliver with us',
+    btnLink: 'Learn more',
+    btnIcon: ArrowRightImg,
+    image: Feature2Img,
+  },
+  feature3: {
+    pretitle: 'Available anytime',
+    title: 'We are available anytime when you need',
+    subtitle:
+      '',
+    btnLink: '',
+    btnIcon: ArrowRightImg,
+    image: Feature3Img,
+  },
+};
+
+export const product = {
+  title: 'Now in Ogun State Abekouta.',
+  subtitle:
+    'No 15 Oluwole street saraki Adigbe, Ogun state Abeokuta.',
+  cards: [
+    {
+      icon: CardIconImg1,
+      title: 'Indoor',
+      subtitle: 'of Ogun State',
+      delay: 200,
+    },
+    {
+      icon: CardIconImg2,
+      title: 'All Areas',
+      subtitle: 'of Ogun State',
+      delay: 400,
+    },
+    {
+      icon: CardIconImg3,
+      title: 'Outdoor',
+      subtitle: 'of Ogun State',
+      delay: 600,
+    },
+  ],
+};
+
+export const pricing = {
+  title: 'Choose your flexible plan.',
+  cards: [
+    {
+      icon: PricingIcon1,
+      title: 'Starter Plan',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$9.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 300,
+    },
+    {
+      icon: PricingIcon2,
+      title: 'Silver Plan',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$19.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 600,
+    },
+    {
+      icon: PricingIcon3,
+      title: 'Diamond Plan',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$29.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 900,
+    },
+  ],
+};
+
+export const testimonials = {
+  title: 'We have Thousands of best wishers',
+  clients: [
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg1,
+      name: 'Cameron Williamson',
+      position: 'CEO',
+      borderColor: '#FF7235',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg2,
+      name: 'Shirley Hand',
+      position: 'CEO',
+      borderColor: '#FFBE21',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg3,
+      name: 'Dr. Olivia Hansen',
+      position: 'CEO',
+      borderColor: '#4756DF',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg4,
+      name: 'Aubrey Sanford',
+      position: 'CEO',
+      borderColor: '#3EC1F3',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg5,
+      name: 'Terri Conroy',
+      position: 'CEO',
+      borderColor: '#BB7259',
+    },
+  ],
+};
+
+export const cta = {
+  title: '20,000+ Delivered merchandise goods across Nigeria',
+  subtitle: 'Treasure logistics Company Limited',
+  btnText: '',
+  img1: CtaImg1,
+  img2: CtaImg2,
+};
+
+export const footer = {
+  logo: 'Treasure Logistics',
+  links: [
+    { name: 'Home', href: '/' },
+    { name: 'About us', href: '/' },
+    // { name: 'Careers', href: '/' },
+    // { name: 'Pricing', href: '/' },
+    { name: 'Features', href: '/' },
+    // { name: 'Blog', href: '/' },
+  ],
+  legal: [
+    { name: 'Our office Address:'},
+    { name: 'No 15 Oluwole street saraki Adigbe, Ogun state Abeokuta.', href: '/' },
+    { name: 'Telephone :'},
+    { name: '+234-905-127-4834 +234-7089862698', href: '/' },
+    { name: 'G-mail:'},
+    { name: 'stephanaduma60@gmail.com', href: '/' },
+  ],
+  newsletter: {
+    title: 'Newsletter',
+    subtitle: 'Over 25000 people have subscribed',
+  },
+  form: {
+    placeholder: 'Enter your email',
+    btnText: 'Subscribe',
+    smallText: 'We don’t sell your email and spam',
+  },
+};
+
+export const copyright = {
+  link1: {
+    name: 'Privacy & Terms',
+    href: '/',
+  },
+  link2: {
+    name: 'Contact us',
+    href: '/',
+  },
+  copyText: 'Copyright @ 2022 damel design',
+  social: [
+    { icon: WhatsappIcon, href: '/' },
+    { icon: GmailIcon, href: '/' },
+    { icon: PhoneIcon, href: '/' },
+  ],
+};
