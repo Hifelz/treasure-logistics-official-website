2421ff91cc8fde7365ccab9256e93cb1470a4148
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-10-12 21:49:06 +0100

    testing

--- src/components/Form.js
+++ src/components/Form.js
@@ -37,6 +37,17 @@ const Form = () => {
             />
           </div>
           <div class="relative mb-4">
+            <label for="full-name" class="leading-7 text-sm text-gray-600">
+              password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
             <label for="number" class="leading-7 text-sm text-gray-600">
               Phone Number
             </label>
